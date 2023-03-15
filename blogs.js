164943f5ab04b02fbcc6691acad26fcd677421b7
prash00b <prashamsa068@gmail.com>
2023-03-15 16:40:33 +0545

    Secocond

---
+++ blogs.js
@@ -0,0 +1,50 @@
+blogs = [
+    {
+      "title": "The Benefits of Yoga for Mind and Body",
+      "author": "Jane Doe",
+      "date": "2022-03-10",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et odio euismod, lobortis purus ac, rhoncus nisi. Integer vel nulla quis risus lobortis ullamcorper. Sed euismod lorem ut nisl bibendum, eu volutpat sem tristique. Praesent id neque at velit hendrerit consequat. Vivamus laoreet vestibulum ante ac egestas. Sed et velit dolor. Donec vel nibh eu mauris aliquam volutpat. Fusce condimentum tellus vel aliquam ultrices. Quisque eleifend ultrices enim, eu blandit orci finibus ut."
+    },
+    {
+      "title": "5 Tips for Productive Remote Work",
+      "author": "John Smith",
+      "date": "2022-03-08",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et odio euismod, lobortis purus ac, rhoncus nisi. Integer vel nulla quis risus lobortis ullamcorper. Sed euismod lorem ut nisl bibendum, eu volutpat sem tristique. Praesent id neque at velit hendrerit consequat. Vivamus laoreet vestibulum ante ac egestas. Sed et velit dolor. Donec vel nibh eu mauris aliquam volutpat. Fusce condimentum tellus vel aliquam ultrices. Quisque eleifend ultrices enim, eu blandit orci finibus ut."
+    },
+    {
+      "title": "How to Make the Perfect Cup of Coffee",
+      "author": "Sara Johnson",
+      "date": "2022-03-06",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et odio euismod, lobortis purus ac, rhoncus nisi. Integer vel nulla quis risus lobortis ullamcorper. Sed euismod lorem ut nisl bibendum, eu volutpat sem tristique. Praesent id neque at velit hendrerit consequat. Vivamus laoreet vestibulum ante ac egestas. Sed et velit dolor. Donec vel nibh eu mauris aliquam volutpat. Fusce condimentum tellus vel aliquam ultrices. Quisque eleifend ultrices enim, eu blandit orci finibus ut."
+    },
+    { 
+      "title": "10 Healthy Eating Habits to Improve Your Life",
+      "author": "Emily Brown",
+      "date": "2022-03-03",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et odio euismod, lobortis purus ac, rhoncus nisi. Integer vel nulla quis risus lobortis ullamcorper. Sed euismod lorem ut nisl bibendum, eu volutpat sem tristique. Praesent id neque at velit hendrerit consequat. Vivamus laoreet vestibulum ante ac egestas. Sed et velit dolor. Donec vel nibh eu mauris aliquam volutpat. Fusce condimentum tellus vel aliquam ultrices. Quisque eleifend ultrices enim, eu blandit orci finibus ut."
+    },
+    {
+      "title": "The Art of Photography: Capturing Moments in Time",
+      "author": "Emily Brown",
+      "date": "2022-03-03",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et odio euismod, lobortis purus ac, rhoncus nisi. Integer vel nulla quis risus lobortis ullamcorper. Sed euismod lorem ut nisl bibendum, eu volutpat sem tristique. Praesent id neque at velit hendrerit consequat. Vivamus laoreet vestibulum ante ac egestas. Sed et velit dolor. Donec vel nibh eu mauris aliquam volutpat. Fusce condimentum tellus vel aliquam ultrices. Quisque eleifend ultrices enim, eu blandit orci finibus ut."
+    },
+    {
+      "title": "10 Healthy Eating Habits to Improve Your Life",
+      "author": "Emily Brown",
+      "date": "2022-03-03",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et odio euismod, lobortis purus ac, rhoncus nisi. Integer vel nulla quis risus lobortis ullamcorper. Sed euismod lorem ut nisl bibendum, eu volutpat sem tristique. Praesent id neque at velit hendrerit consequat. Vivamus laoreet vestibulum ante ac egestas. Sed et velit dolor. Donec vel nibh eu mauris aliquam volutpat. Fusce condimentum tellus vel aliquam ultrices. Quisque eleifend ultrices enim, eu blandit orci finibus ut."
+    },
+    {
+      "title": "10 Healthy Eating Habits to Improve Your Life",
+      "author": "Emily Brown",
+      "date": "2022-03-03",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et odio euismod, lobortis purus ac, rhoncus nisi. Integer vel nulla quis risus lobortis ullamcorper. Sed euismod lorem ut nisl bibendum, eu volutpat sem tristique. Praesent id neque at velit hendrerit consequat. Vivamus laoreet vestibulum ante ac egestas. Sed et velit dolor. Donec vel nibh eu mauris aliquam volutpat. Fusce condimentum tellus vel aliquam ultrices. Quisque eleifend ultrices enim, eu blandit orci finibus ut."
+    },
+    {
+      "title": "10 Healthy Eating Habits to Improve Your Life",
+      "author": "Emily Brown",
+      "date": "2022-03-03",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et odio euismod, lobortis purus ac, rhoncus nisi. Integer vel nulla quis risus lobortis ullamcorper. Sed euismod lorem ut nisl bibendum, eu volutpat sem tristique. Praesent id neque at velit hendrerit consequat. Vivamus laoreet vestibulum ante ac egestas. Sed et velit dolor. Donec vel nibh eu mauris aliquam volutpat. Fusce condimentum tellus vel aliquam ultrices. Quisque eleifend ultrices enim, eu blandit orci finibus ut."
+    }
+]
